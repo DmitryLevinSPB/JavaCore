729338ab3ccbac43ac73d566bf5be57bd0856416
Levin <Levushka22@yandex.ru>
2026-02-11 15:37:25 +0300

    added a lesson on string

--- src/main/java/String/main.java
+++ src/main/java/String/main.java
@@ -21,14 +21,19 @@ public class main {
         String str6 = str1 + "?";
         String str7 = "Hello?";
         String str8 = "Hellohello";
+        String s = "Java langu3424age very go3434od";
+        String r = "JavaVeryGood";
+        String c = "Java the best   ";
+
+
         //    str1 += "!"; // если сравнивать с str2 будет false  т.к после добавления ! стринг попадает в хип а не в стринг пул
 
 
-        //System.out.println(str1==str3); // false
+        // System.out.println(str1==str3); // false
         // System.out.println(str1==str2); // true т.к. канкатенация дает то-же слово, что лежит в стринг пуле
         // System.out.println(str1==str4); // true
-        //System.out.println(str1==str5); // false  регистро зависимость
-        //  System.out.println(str1.equals(str3)); // true т.к equals сравнивает посимвольно, а не ссылки
+        // System.out.println(str1==str5); // false  регистро зависимость
+        // System.out.println(str1.equals(str3)); // true т.к equals сравнивает посимвольно, а не ссылки
         // System.out.println(str7==str6); // false
 
 
@@ -38,30 +43,39 @@ public class main {
             s += "A";
         }
         System.out.println(s);*/
-        // Методы класса
-        // System.out.println(str7.length()); // возвращает длинну строки
-        //System.out.println(str1.charAt(2));// вернет символ который находится на заданной позиции(2)
-        //System.out.println(str7.toCharArray()); // преобразование строки вмассив символов
-        //System.out.println(str1.equals(str2)); // сравнивает два объекта булиново значение
-        //System.out.println(str4.equalsIgnoreCase(str5)); // сравнивает без учета регистра
-        //System.out.println(str1.concat(str7)); // объединяет строки
-        //System.out.println(str1.toUpperCase(Locale.ROOT));
-        // System.out.println(str1.toLowerCase(Locale.ROOT));
-        //System.out.println(str2.getBytes(StandardCharsets.UTF_8)); // получает байты
 
-        //System.out.println(str1.matches("\\w+")); // можно проверить например состоит ли строка только из букв
-        // System.out.println(str1.matches("\\w{5}"));// проверит состоит ли слово из 5 букв
-        //  System.out.println(str1.matches("^H\\w+o$")); // Проверит начинается ли слово с H и заканчивается o
 
-        String s = "Java langu3424age very go3434od";
-        String r = "JavaVeryGood";
-       // String [] string = s.split(" ", 2);
-      //  s = s.replace(" ", "+"); // поменяет пробелы на +
-        s = s.replaceAll("\\d", "*"); // поменяет все цифры
-        r= r.replaceAll("(^\\w)(\\w+)(\\w$)", "$2$3$1");
-      //  System.out.println(Arrays.toString(string));
-        System.out.println(r);
+        // --- Методы класса ---
 
+        // System.out.println(str7.length());                           // возвращает длинну строки
+        // System.out.println(str1.charAt(2));                          // вернет символ который находится на заданной позиции(2)
+        // System.out.println(str7.toCharArray());                      // преобразование строки вмассив символов
+        // System.out.println(str1.equals(str2));                       // сравнивает два объекта булиново значение
+        // System.out.println(str4.equalsIgnoreCase(str5));             // сравнивает без учета регистра
+        // System.out.println(str1.concat(str7));                       // объединяет строки
+        // System.out.println(str1.toUpperCase(Locale.ROOT));           // переведет все символы в верхний
+        // System.out.println(str1.toLowerCase(Locale.ROOT));           // переведет все символы в нижний регистр
+        // System.out.println(str2.getBytes(StandardCharsets.UTF_8));   // получает байты
+
+        // System.out.println(str1.matches("\\w+"));                    // можно проверить например состоит ли строка только из букв
+        // System.out.println(str1.matches("\\w{5}"));                  // проверит состоит ли слово из 5 букв
+        // System.out.println(str1.matches("^H\\w+o$"));                // проверит начинается ли слово с H и заканчивается o
+
+
+        // String [] string = s.split(" ", 2);                 // ставит разделитель в указанных местах. Можно установить лимит
+        //  System.out.println(Arrays.toString(string));
+
+        // s = s.replace(" ", "+");                                     // поменяет пробелы на +
+        // s = s.replaceAll("\\d", "*");                                // поменяет все цифры
+        // r= r.replaceAll("(^\\w)(\\w+)(\\w$)", "$2$3$1");             // поменяет местами части текста
+        // System.out.println(r);
+
+        // System.out.println(c.substring(5));                           // подстрока выведет символы начиная с 5 символа
+        // System.out.println(c.substring(5,8));                         // подстрока выведет символы с 5 по 8
+        // System.out.println(c.trim());                                 // удаляет пробельные символы
+        // System.out.println(c.strip());                                // так же удаляет символы
+
+        // У строки есть статические методы которые не требуют создание объекта
 
 
     }
